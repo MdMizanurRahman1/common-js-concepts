b9969c752f1e171ba9165db0a69337aada643049
MdMizanurRahman1 <mizan.rahman2217@gmail.com>
2022-09-08 13:04:31 +0200

    double vs triple equal

---
+++ triple-equal.js
@@ -0,0 +1,11 @@
+//always use ===
+//equal comparison does not work for non primitive
+const first = 1;
+const second = true;
+
+if (first === second) {
+    console.log('values are equal')
+}
+else {
+    console.log('values are not equal')
+}
